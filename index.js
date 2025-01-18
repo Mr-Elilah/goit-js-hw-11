7e1f5ade7a0129923e87e6d517938c80f6f1b541
Mr-Elilah <beztalannyj.ilia@gmail.com>
2025-01-18 23:12:34 +0000

    Deploying to gh-pages from @ Mr-Elilah/goit-js-hw-11@b5091825b20fe8d5ce4750f38aed39f6dbb4c877 🚀

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-import{i as u}from"./assets/vendor-17o45ynk.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();const l=document.querySelector(".js-search-form"),a=o=>{if(o.preventDefault(),o.currentTarget.elements.user_query.value.trim()===""){u.error({title:"Error",message:"Enter your request",position:"topRight"});return}};l.addEventListener("submit",a);const t=document.querySelector(".search-input");t.addEventListener("input",()=>{t.value.trim()!==""?(t.classList.remove("empty"),t.classList.add("filled")):(t.classList.remove("filled"),t.classList.add("empty"))});
+import{i as u,S as h}from"./assets/vendor-5ObWk2rO.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const p="https://pixabay.com/api/",y="48226781-c314bf294542f2e13595e23de";function g(t){const s=`${p}?key=${y}&q=${t}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(s).then(r=>{if(!r.ok)throw new Error("Failed to fetch images");return r.json()}).then(r=>r.hits).catch(r=>{throw new Error(r.message)})}function c(t){u.error({title:"Error",message:t})}function L(t){u.success({title:"Success",message:t})}function b(t){const s=document.querySelector(".gallery"),r=t.map(({webformatURL:i,largeImageURL:e,tags:o,likes:a,views:d,comments:f,downloads:m})=>`
+        <li class="gallery-item">
+          <a href="${e}">
+            <img src="${i}" alt="${o}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes:</b> ${a}
+              </p>
+              <p class="info-item">
+                <b>Views:</b> ${d}
+              </p>
+              <p class="info-item">
+                <b>Comments:</b> ${f}
+              </p>
+              <p class="info-item">
+                <b>Downloads:</b> ${m}
+              </p>
+            </div>
+          </a>
+        </li>
+      `).join("");s.innerHTML=r}function w(){const t=document.querySelector(".loader");t?t.classList.add("show"):console.error("Loader not found!")}function l(){const t=document.querySelector(".loader");t?t.classList.remove("show"):console.error("Loader not found!")}const S=document.querySelector(".search-form"),v=document.querySelector(".search-input");S.addEventListener("submit",q);function q(t){t.preventDefault();const s=v.value.trim();if(s===""){c("Please enter a search query.");return}n.value="",localStorage.clear(),w(),g(s).then(r=>{if(l(),r.length===0){c("Sorry, there are no images matching your search query. Please try again!");return}b(r),L("Images loaded successfully!"),new h(".gallery a",{captionsData:"alt",captionDelay:250}).refresh()}).catch(r=>{l(),c("Oops, something went wrong. Please try again later.")})}const n=document.querySelector(".search-input");n.addEventListener("input",()=>{n.value.trim()!==""?(n.classList.remove("empty"),n.classList.add("filled")):(n.classList.remove("filled"),n.classList.add("empty"))});
 //# sourceMappingURL=index.js.map
